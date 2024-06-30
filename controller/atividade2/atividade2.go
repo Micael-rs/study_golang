dc90d67035c8befdd20d924499ec3d894e425681
Natana <naelramosfilho@gmail.com>
2024-06-30 17:35:30 -0300

    Adiciona sintaxe de ARRAY e ESTRUCT na pasta atividade2

--- controller/atividade2/atividade2.go
+++ controller/atividade2/atividade2.go
@@ -15,6 +15,7 @@ func Ponteiro() int {
 
 func Vertex() {
 
+	//
 	type Vertex struct {
 		Valor1 int
 		Valor2 int
@@ -29,5 +30,23 @@ func Vertex() {
 	p.Valor2 = 8
 
 	fmt.Println(v.Valor2)
+
+}
+
+func Arrays() {
+
+	//Declarando uma Array
+	var a [5]int
+	a[1] = 1
+	fmt.Println(a)
+
+	//manipulando uma array
+	b := [5]int{}
+	fmt.Println(b)
+
+	//Criando uma fatia
+	array := [5]int{1, 2, 3, 4, 5}
+	var c []int = array[1:4]
+	fmt.Println(c)
 
 }
